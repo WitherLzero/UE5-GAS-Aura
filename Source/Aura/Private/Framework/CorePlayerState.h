717e0b23f44e5b59c2bc5410b3efe4bebf17440e
rynnli <witherlzero@gmail.com>
2025-12-31 20:03:16 +0800

    Create CorePlayerState class

---
+++ Source/Aura/Private/Framework/CorePlayerState.h
@@ -0,0 +1,16 @@
+﻿// Copyright rynnli
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "CorePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ACorePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+};

39c0769c51dcd5fc1153511f9ddf8687361da9da
rynnli <witherlzero@gmail.com>
2026-01-02 12:03:05 +0800

    Add Health and Mana attributes in attribute set with Replication support

--- Source/Aura/Private/AbilitySystem/CoreAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/CoreAttributeSet.cpp
@@ -2,3 +2,44 @@
 
 
 #include "AbilitySystem/CoreAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UCoreAttributeSet::UCoreAttributeSet()
+{
+}
+
+void UCoreAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(UCoreAttributeSet,Health,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCoreAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCoreAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCoreAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+		
+
+}
+
+void UCoreAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCoreAttributeSet,Health,OldHealth);
+	
+}
+
+void UCoreAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCoreAttributeSet,MaxHealth,OldMaxHealth);
+}
+
+void UCoreAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCoreAttributeSet,Mana,OldMana);
+}
+
+void UCoreAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCoreAttributeSet,MaxMana,OldMaxMana);
+}
+
+
--- Source/Aura/Public/AbilitySystem/CoreAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/CoreAttributeSet.h
@@ -5,7 +5,6 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "CoreAttributeSet.generated.h"
-
 /**
  * 4
  */
@@ -13,4 +12,34 @@ UCLASS()
 class AURA_API UCoreAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	
+public:
+	UCoreAttributeSet();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+
 };

be16715dfb94a67b2ea54998df416a5cf85914c1
rynnli <witherlzero@gmail.com>
2026-01-02 14:58:01 +0800

    Create EffectActor class with overlap logic and basic health adjustment implementation

---
+++ Source/Aura/Private/Actor/EffectActor.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright rynnli
+
+
+#include "Actor/EffectActor.h"
+
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/CoreAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+
+AEffectActor::AEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+	
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+void AEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Sphere->OnComponentBeginOverlap.AddDynamic(this,&AEffectActor::OnOverlap);
+	
+}
+
+void AEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// TODO: Need to change this to apply Gameplay Effect. 
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UCoreAttributeSet* CoreAttributeSet = Cast<UCoreAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UCoreAttributeSet::StaticClass()));
+		UCoreAttributeSet* MutableCAS = const_cast<UCoreAttributeSet*>(CoreAttributeSet);
+		MutableCAS->SetHealth(CoreAttributeSet->GetHealth() + 20.f);
+		
+		Destroy();
+	
+	}
+}
+
---
+++ Source/Aura/Public/Actor/EffectActor.h
@@ -0,0 +1,32 @@
+﻿// Copyright rynnli
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class AURA_API AEffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEffectActor();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+	
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
+};

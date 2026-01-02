7add8ee736b2b9576b5e57059c9d5765d9e64f69
rynnli <witherlzero@gmail.com>
2026-01-02 22:09:43 +0800

    Create AuraHUD class and add overlay widget to viewport

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright rynnli
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (OverlayWidgetClass)
+	{
+		OverlayWidget = CreateWidget<UAuraUserWidget>(GetWorld(), OverlayWidgetClass);
+	}
+
+	if (OverlayWidget)
+	{
+		OverlayWidget->AddToViewport();
+	}
+}
---
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,29 @@
+﻿// Copyright rynnli
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+
+};

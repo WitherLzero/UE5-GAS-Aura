d70cb0cdd5ef73f233816bef8e409e1ea38d051f
rynnli <witherlzero@gmail.com>
2026-01-23 23:36:32 +0800

    Add projectile collision channel and adjust collision configuration

--- Source/Aura/Private/Framework/CoreCharacterBase.cpp
+++ Source/Aura/Private/Framework/CoreCharacterBase.cpp
@@ -6,6 +6,7 @@
 #include "AbilitySystemComponent.h"
 #include "InputActionValue.h"
 #include "AbilitySystem/CoreAbilitySystemComponent.h"
+#include "Aura/Aura.h"
 #include "Components/CapsuleComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
 
@@ -15,6 +16,8 @@ ACoreCharacterBase::ACoreCharacterBase()
 	
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera,ECR_Ignore);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Camera,ECR_Ignore);
+	GetMesh()->SetCollisionResponseToChannel(ECC_Projectile,ECR_Overlap);
+	GetMesh()->SetGenerateOverlapEvents(true);
 	
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
 	Weapon->SetupAttachment(GetMesh(),FName("WeaponSocket"));

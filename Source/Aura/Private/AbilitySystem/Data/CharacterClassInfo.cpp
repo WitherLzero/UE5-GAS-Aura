d97df422c09cbafa3f7612a240265ccd24ce9c1e
rynnli <witherlzero@gmail.com>
2026-01-24 21:50:45 +0800

    Add Character class info and create data asset

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright rynnli
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInfo.FindChecked(CharacterClass);
+}

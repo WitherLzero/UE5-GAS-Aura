61ad1fb3ca7bba61a07fcf6cd8486057336a46f7
rynnli <witherlzero@gmail.com>
2026-01-30 15:15:25 +0800

    Create custom gameplay effect context struct

---
+++ Source/Aura/Private/RPGAbilityTypes.cpp
@@ -0,0 +1,6 @@
+﻿#include "RPGAbilityTypes.h"
+
+bool FRPGGameplayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess)
+{
+	return FGameplayEffectContext::NetSerialize(Ar, Map, bOutSuccess);
+}

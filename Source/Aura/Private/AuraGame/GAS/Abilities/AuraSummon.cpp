f9525ec111197efd346eac9b8b76f4a84b8adfbb
rynnli <witherlzero@gmail.com>
2026-02-15 14:05:44 +0800

    Add Shaman new ability summon and Create the function to generate Summon locations

---
+++ Source/Aura/Private/AuraGame/GAS/Abilities/AuraSummon.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright rynnli
+
+
+#include "AuraGame/GAS/Abilities/AuraSummon.h"
+
+TArray<FVector> UAuraSummon::GetSummonLocations()
+{
+	TArray<FVector> SummonLocations;
+	
+	const FVector ActorLoc = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const float DeltaSpread = SpawnSpread / NumMinions;
+	
+	const FVector LeftOfSpread = Forward.RotateAngleAxis( -SpawnSpread/2.f, FVector::UpVector);
+	for (int i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis( DeltaSpread*i, FVector::UpVector);
+		FVector SpawnLocation = ActorLoc + Direction * FMath::FRandRange(MinSpawnDist,MaxSpawnDist);
+		
+		FHitResult HitResult;
+		GetWorld()->LineTraceSingleByChannel(HitResult,
+			SpawnLocation + 300.f * FVector::UpVector,SpawnLocation + 300.f * FVector::DownVector,ECollisionChannel::ECC_Visibility);
+		if (HitResult.bBlockingHit)
+		{
+			SpawnLocation = HitResult.ImpactPoint;
+		}
+		
+		SummonLocations.Add(SpawnLocation);
+	}
+	
+	return SummonLocations;
+}
---
+++ Source/Aura/Public/AuraGame/GAS/Abilities/AuraSummon.h
@@ -0,0 +1,36 @@
+﻿// Copyright rynnli
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RPGFramework/GAS/Abilities/RPGGameplayAbilityBase.h"
+#include "AuraSummon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraSummon : public URPGGameplayAbilityBase
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSummonLocations();
+	
+protected:
+	UPROPERTY(EditDefaultsOnly,Category="Summoning")
+	int32 NumMinions = 5;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Summoning")
+	float MinSpawnDist = 100.f;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Summoning")
+	float MaxSpawnDist = 400.f;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Summoning")
+	float SpawnSpread = 90.f;
+};

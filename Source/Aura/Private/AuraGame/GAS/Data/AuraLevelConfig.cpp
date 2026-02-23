88449a7a8e9725a649331dbbc2b7e26928519177
rynnli <witherlzero@gmail.com>
2026-02-23 12:42:39 +0800

    Add level config Data asset

---
+++ Source/Aura/Private/AuraGame/GAS/Data/AuraLevelConfig.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright rynnli
+
+
+#include "AuraGame/GAS/Data/AuraLevelConfig.h"
+
+int32 UAuraLevelConfig::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// LevelUpInformation[1] = Level 1 Information
+		// LevelUpInformation[2] = Level 2 Information
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
